c99319ab0d6f94ad959231b42b846bbf2cdec069
Cinar <cinar-ibrahim@outlook.com>
2024-11-29 12:16:31 +0300

    Proje dosyası ekle.

---
+++ LibraryManagementSystemWithJwtToken/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LibraryManagementSystemWithJwtToken.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
